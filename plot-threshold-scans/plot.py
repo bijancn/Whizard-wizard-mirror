9b13cdb7a12e2a2d13276f7057500de0497fcb1e
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-05-07 12:57:27 +0200

    Adding christians plots

--- plot-threshold-scans/plot.py
+++ plot-threshold-scans/plot.py
@@ -6,11 +6,14 @@ colors = ['#EE3311',  # red
           '#3366FF',  # blue
           '#109618',  # green
           '#FF9900',  # orange
-          '#990099']  # lilac
+          '#990099',  # lilac
+          '#000000']  # black
 
 files = ['./ttbar_lo_onshell.dat', './ttbar_nlo_onshell.dat',
-         './ttbar_lo_offshell.dat', './ttbar_lo_offshell_restricted.dat']
-labels = ['LO Onshell', 'NLO Onshell', 'LO Offshell', 'LO Offshell Restricted']
+         './ttbar_lo_offshell.dat', './ttbar_lo_offshell_restricted.dat',
+         './christian_scan_total_lo.dat', './christian_scan_total_nlo.dat']
+labels = ['LO Onshell', 'NLO Onshell', 'LO Offshell', 'LO Offshell Restricted',
+          'Christian LO Offshell', 'Christian NLO Offshell']
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 data = [np.loadtxt(filename, unpack=True) for filename in files]
@@ -25,6 +28,7 @@ xmin = min([np.amin(d[0]) for d in data])
 xmax = max([np.amax(d[0]) for d in data])
 ymin = min([np.amin(d[1]) for d in data])
 ymax = max([np.amax(d[1]) for d in data])
+xmax = 500
 
 pl = Plotter()
 pl.setfig(ax, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax,
