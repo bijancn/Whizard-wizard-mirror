74a0189490a333d206a5d3ada61baa842b8f2809
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-06-04 17:25:20 +0200

    Align NLO_FixedOrder directory structure and factor out default build rules

---
+++ cleanrules.mk
@@ -0,0 +1,9 @@
+# `make clean` removes all derived data. Be careful what you wish for.
+# You can create less disruptive clean rules here
+
+clean :
+	rm -rf */*.mod */*.f90 */*.o */*.lo */*.vg */*.evx */*.phs */*.log */*.lhe */*.hepmc
+	rm -rf */*.olp_parameters */fort.* */stability_log */test_sudakov_data
+	rm -rf */*.olp */*.olc */*.la */Generated_Loops */*olp_modules */*.so */lib
+	rm -rf */include */.libs/ */fort.7 */golem.in */Gosam_Makefile */*makefile
+	rm -rf */gmon.out */*.dat */*.yoda */plots
