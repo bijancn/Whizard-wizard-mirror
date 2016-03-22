74b81f0c63f892e629f90284f4a725c1edfab5a0
Chrisitan Weiss <christian.weiss@desy.de>
2016-03-22 13:51:25 +0100

    Remove comments

--- mpi_generate.py
+++ mpi_generate.py
@@ -77,15 +77,6 @@ def setup_sindarins(proc_dict, batch=None):
               proc_dict['adaption_iterations'], proc_dict.get('integration_iterations', ' '))
           subproc.multiply_sindarins (integration_sindarin, template_sindarin, 
              proc_dict.get('scale_variation', False), proc_dict['nlo_type'])
-          #scaled_sindarins = None
-          #if proc_dict.get('scale_variation', False):
-          #  scaled_sindarins = subproc.create_scale_sindarins (integration_sindarin)
-          #if proc_dict['nlo_type'] == 'nlo':
-          #  if scaled_sindarins != None:
-          #    for sindarin in scaled_sindarins:
-          #      subproc.create_nlo_component_sindarins(sindarin)
-          #  else:
-          #    subproc.create_nlo_component_sindarins(integration_sindarin)
         elif proc_dict['purpose'] == 'histograms' or proc_dict['purpose'] == 'events':
           subproc.create_simulation_sindarin(base_sindarin, template_sindarin,
               proc_dict['process'], proc_dict['adaption_iterations'], 
@@ -93,16 +84,6 @@ def setup_sindarins(proc_dict, batch=None):
               proc_dict['events_per_batch']) 
           subproc.multiply_sindarins (base_sindarin, 
              proc_dict.get('scale_variation', False), proc_dict['nlo_type'])
-          ### Evil code duplication
-          #scaled_sindarins = None
-          #if proc_dict.get('scale_variation', False):
-          #  scaled_sindarins = subproc.create_scale_sindarins (base_sindarin)
-          #if proc_dict['nlo_type'] == 'nlo':
-          #  if scaled_sindarins != None:
-          #    for sindarin in scaled_sindarins:
-          #      subproc.create_nlo_component_sindarins(sindarin)
-          #  else:
-          #    subproc.create_nlo_component_sindarins(base_sindarin)
 
   else:
     logger.info('Skipping ' + proc_dict['process'] + ' because it is disabled')
