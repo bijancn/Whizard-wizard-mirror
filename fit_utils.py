797c58804c526cfe7c6821886fea470794d57343
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-05-12 19:17:37 +0200

    Add fit_utils.py

---
+++ fit_utils.py
@@ -0,0 +1,14 @@
+# Various tools for fitting functions to our data
+# Needs statsmodels which can be installed by checking out the following repo:
+# git clone git://github.com/statsmodels/statsmodels.git
+import statsmodels.api as sm
+import numpy as np
+
+
+def fit_line(x, y, verbose=False):
+  regression = sm.OLS(y, sm.add_constant(x)).fit()
+  if verbose:
+    print regression.summary()
+  fit_x = np.linspace(min(x), max(x), 100)
+  fit_y = fit_x * regression.params[1] + regression.params[0]
+  return fit_x, fit_y
