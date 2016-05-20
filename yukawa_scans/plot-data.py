3c138c43d2f87653bc95ac5232c5a06143252807
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-05-20 14:32:29 +0200

    Add yukawa scans

---
+++ yukawa_scans/plot-data.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import sys
+import os
+import glob
+import multiprocessing as mp
+import inspect
+from functools import partial
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+import bcn_plot
+from utils import load_json
+import data_utils
+
+
+def ls_decider(lbl, title):
+  return "solid"
+
+
+def pretty_label(l, title):
+  l = os.path.basename(l)
+  l = l.replace('proc_lo', '$t \\bar t$')
+  l = l.replace('proc_nlo', '$t \\bar t$')
+  l = l.replace('.dat', '')
+  return l
+
+
+def plot_x_axis(ax, title):
+  ax.axhline(0)
+  return ax
+
+
+def main():
+  pic_path = os.path.abspath('./plots') + '/'
+  data_path = os.path.abspath('./scan-results')
+  files = glob.glob(data_path + '/*.dat')
+  plot_json = load_json('plot.json')
+  plot_dict = plot_json['plots']
+  data = data_utils.load_and_clean_files(files, plot_json.get('smooth', None))
+  pool = mp.Pool(processes=3)
+  plot_this = partial(bcn_plot.plot,
+                      plot_extra=plot_x_axis, data=data, pic_path=pic_path,
+                      linestyle_decider=ls_decider, pretty_label=pretty_label)
+  pool.map(plot_this, plot_dict)
+
+main()
