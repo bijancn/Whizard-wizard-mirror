4d7ab6eaf1a8ab045433777f4b7422ecf305c15b
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-05-27 11:22:02 +0200

    Work towards a generic SConstruct

--- sconstruct_utils.py
+++ sconstruct_utils.py
@@ -7,18 +7,53 @@ def filter_existing_processes(proc_list, suffix):
   return filter(lambda p: os.path.isfile(p + '-0.' + suffix), proc_list)
 
 
-def get_yodas_from_proc_list(proc_list):
-  return [glob(p + '-*.yoda') for p in proc_list]
-
-
 def get_yodas_from_hepmc(hepmcs):
-  yodas = []
   replace_hepmc = lambda s : re.sub('.hepmc', '.yoda', str(s))
-  for hepmc in hepmcs:
-    yodas.append(map(replace_hepmc, hepmc))
+  return map(replace_hepmc, hepmcs)
+
+
+def get_yodas_from_proc_list(proc_list):
+  yodas = []
+  for process in proc_list:
+    hepmcs = glob(process + '-*.hepmc')
+    if len(hepmcs) > 0:
+      yodas.append(get_yodas_from_hepmc(hepmcs))
+    else:
+      yodas.append(glob(process + '-*.yoda'))
   return yodas
 
 
 def get_final_yoda_names(base_names):
-  remove_proc_ids = lambda s : re.sub('-[0-9]+.yoda', '.yoda', str(s[0]))
-  return map(remove_proc_ids, base_names)
+  results = []
+  for s in base_names:
+    try:
+      results.append(re.sub('-[0-9]+.yoda', '.yoda', str(s[0])))
+    except IndexError:
+      pass
+  return results
+
+
+# TODO: (bcn 2016-05-26) Add test
+
+def find_scale_variation_yodas(merged_yodas):
+  scale_variation_yodas = {
+      'proc_lo_low_yodas': [],
+      'proc_lo_central_yodas': [],
+      'proc_lo_high_yodas': [],
+      'proc_nlo_low_yodas': [],
+      'proc_nlo_central_yodas': [],
+      'proc_nlo_high_yodas': []}
+  for yoda in merged_yodas:
+      if 'nlo_low' in str(yoda):
+        scale_variation_yodas['nlo_low'].append(yoda)
+      elif 'nlo_central' in str(yoda):
+        scale_variation_yodas['proc_nlo_central'].append(yoda)
+      elif 'nlo_high' in str(yoda):
+        scale_variation_yodas['proc_nlo_high'].append(yoda)
+      elif 'lo_low' in str(yoda):
+        scale_variation_yodas['proc_lo_low'].append(yoda)
+      elif 'lo_central' in str(yoda):
+        scale_variation_yodas['proc_lo_central'].append(yoda)
+      elif 'lo_high' in str(yoda):
+        scale_variation_yodas['proc_lo_high'].append(yoda)
+  return scale_variation_yodas
--- site_init.py
+++ site_init.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 from SCons.Script import Builder
+from glob import glob
+import shutil
 
 
 def generate_plot(source, target, env, for_signature):
+  # ' --no-ratio' could be made an option
   plot_cmd = 'rivet-mkhtml --mc-errs --pdf --cm --single ' + \
-      '--ignore-unvalidated --num-threads=$NUMTHREADS --config=$CONFIG --no-ratio '
+      '--ignore-unvalidated --num-threads=$NUMTHREADS --config=$CONFIG '
   target_dir = target[0].dir
   try:
-    descriptions = env['DESCRIPTIONS'].split('::')
+    descriptions = env['DESCRIPTIONS']
   except KeyError:
     descriptions = None
   if descriptions is not None:
@@ -18,16 +21,46 @@ def generate_plot(source, target, env, for_signature):
   return plot_cmd + '-o %s %s' % (target_dir, sources)
 
 
-def generate_plot_with_errors(source, target, env, for_signature):
+def generate_plot_with_lo_line_and_nlo_band(source, target, env, for_signature):
   return 'rivet-mkhtml -c $CONFIG %s:"Title=LO:LineColor=blue" \
      %s:"Title=NLO:ErrorBands=0:LineColor=red" \
      %s:"Title=:ErrorBands=1:ErrorBandColor=red:ErrorBandOpacity=0.5:LineColor=red"' \
      % (source[0], source[1], source[2])
 
 
+def build_scale_variation_yodas(scale_variation_yodas, env):
+  final_nlo_yodas = []
+  for proc_string in ['proc_nlo_low', 'proc_nlo_central', 'proc_nlo_high']:
+    if proc_string in scale_variation_yodas:
+      print proc_string, scale_variation_yodas[proc_string]
+      env.MergeYodasNoScale(target=proc_string + '.yoda',
+          source=scale_variation_yodas[proc_string])
+      final_nlo_yodas.append(proc_string)
+  return final_nlo_yodas
+
+
+def build_yodas_from_hepmcs(env, proc_list):
+  hepmcs = [glob(p + '-*.hepmc') for p in proc_list]
+  print 'Building Yodas from these hepmcs:', hepmcs
+  for h in hepmcs:
+    for hh in h:
+      env.Yoda(hh)
+
+
+def build_merged_yodas(env, small_yoda_names, final_yoda_names):
+  merged_yodas = []
+  for small_yoda, yoda in zip(small_yoda_names, final_yoda_names):
+    if len(small_yoda) > 1:
+      merged_yodas.append(env.MergeYodas(target=yoda, source=small_yoda))
+    else:
+      shutil.copyfile(str(small_yoda[0]), yoda)
+      merged_yodas.append(yoda)
+  return merged_yodas
+
+
 def ADD_BUILDERS(env):
   build_yoda_from_hepmc = Builder(
-      action='rivet --quiet --pwd -H $TARGET $ANALYSIS $SOURCES',
+      action='rivet --quiet --pwd -H $TARGET -a $ANALYSIS $SOURCES',
       suffix='.yoda', src_suffix='.hepmc')
 
   build_analysis = Builder(action='rivet-buildplugin $SOURCES',
@@ -43,9 +76,10 @@ def ADD_BUILDERS(env):
   build_envelope = Builder(action='./yodaenvelopes -o $TARGET -c $SOURCE $SOURCES',
      suffix='.yoda', src_suffix='.yoda')
 
-  build_plot_with_errors = Builder(generator=generate_plot_with_errors,
-                                   suffix='.html',
-                                   src_suffix='.yoda')
+  build_plot_with_lo_line_and_nlo_band = \
+      Builder(generator=generate_plot_with_lo_line_and_nlo_band,
+          suffix='.html',
+          src_suffix='.yoda')
 
   build_plot = Builder(generator=generate_plot,
                       suffix='.html',
@@ -54,4 +88,4 @@ def ADD_BUILDERS(env):
   env.Append(BUILDERS={'Yoda' : build_yoda_from_hepmc, 'Analysis' : build_analysis,
     'MergeYodas': merge_yodas, 'MergeYodasNoScale': merge_yodas_noscale,
     'BuildEnvelope': build_envelope,
-    'Plot' : build_plot, 'PlotWithErrorBand': build_plot_with_errors})
+    'Plot' : build_plot, 'PlotLOLineWithNLOBand': build_plot_with_lo_line_and_nlo_band})
