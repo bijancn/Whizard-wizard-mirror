55f88c51c735a4d5c057d2b8dab2a06d28015187
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-08-15 10:21:01 +0200

    export_www should be a soft link to the top level. git does not really support this

--- ee_WbWbH_scan/export_www.sh
+++ ee_WbWbH_scan/export_www.sh
@@ -1,11 +1,1 @@
-target=~/www/$(basename `pwd`)
-echo "Writing to $target"
-mkdir -p $target
-cp plots/*.pdf $target/
-cp plots/*.svg $target/
-
-rm -f $target/overview.html
-touch $target/overview.html
-for i in plots/*.svg; do
-  echo "<a href=\"$(basename ${i%%.*}).pdf\"><img src=\"$(basename $i)\" alt=\"$(basename $i)\"></a>" >> $target/overview.html
-done
+../export_www.sh
