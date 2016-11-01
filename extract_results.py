7c711207792a8da9d8e2e6897e26e51930023e7e
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-11-01 15:27:39 +0100

    Find out process folder automatically in `extract_results`

--- extract_results.py
+++ extract_results.py
@@ -3,7 +3,7 @@ import sys
 import os
 import subprocess
 import whizard_wizard
-from utils import cd, mkdirs, fatal
+from utils import mkdirs
 
 
 def get_RES(c):
@@ -29,36 +29,30 @@ def build_regexs(proc):
 
 
 def main():
-  try:
-    process_folder = sys.argv[1]
-  except:
-    fatal('You have to give me the process directory as argument')
-  else:
-    with cd(process_folder):
-      result_path = 'scan-results'
-      mkdirs(result_path)
-      run_json = whizard_wizard.retrieve_and_validate_run_json('.')
-      process_runs = whizard_wizard.fill_all_runs(run_json)
-      # TODO: (bcn 2016-07-15) for now we only support separate export
-      # 2D export could be relevant as well
-      process_names = []
-      for proc in process_runs:
-        if proc[2]['purpose'] == 'scan':
-          process_names += build_regexs(proc)
-      process_names = list(set(process_names))
-      runfolders = ['whizard/' + p for p in process_names]
-      for rf in runfolders:
-        print 'runfolders = ', rf
-      result_cmd = [get_RES(runfolder) for runfolder in runfolders]
-      result_files = [os.path.join(result_path, p.replace('-*', '') + '.dat')
-          for p in process_names]
-      print 'result_files = ', result_files
-      for cmd, file_name in zip(result_cmd, result_files):
-        ret = subprocess.call(cmd + ' > ' + file_name, shell=True)
-        if (ret == 0):
-          print 'Saved to ' + file_name
-        else:
-          print 'Saving to ' + file_name + ' returned ' + str(ret)
-          sys.exit(1)
+  result_path = 'scan-results'
+  mkdirs(result_path)
+  run_json = whizard_wizard.retrieve_and_validate_run_json('.')
+  process_runs = whizard_wizard.fill_all_runs(run_json)
+  # TODO: (bcn 2016-07-15) for now we only support separate export
+  # 2D export could be relevant as well
+  process_names = []
+  for proc in process_runs:
+    if proc[2]['purpose'] == 'scan':
+      process_names += build_regexs(proc)
+  process_names = list(set(process_names))
+  runfolders = ['whizard/' + p for p in process_names]
+  for rf in runfolders:
+    print 'runfolders = ', rf
+  result_cmd = [get_RES(runfolder) for runfolder in runfolders]
+  result_files = [os.path.join(result_path, p.replace('-*', '') + '.dat')
+      for p in process_names]
+  print 'result_files = ', result_files
+  for cmd, file_name in zip(result_cmd, result_files):
+    ret = subprocess.call(cmd + ' > ' + file_name, shell=True)
+    if (ret == 0):
+      print 'Saved to ' + file_name
+    else:
+      print 'Saving to ' + file_name + ' returned ' + str(ret)
+      sys.exit(1)
 
 main()
